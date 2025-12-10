cc7ec784954b320e033e896670c650f16eeee49f
AlmantasK <almantusk@gmail.com>
2025-12-11 00:37:49 +0200

    part2 full (with mod)

--- AdventOfCode2025/src/day1/Part2.kt
+++ AdventOfCode2025/src/day1/Part2.kt
@@ -10,52 +10,44 @@ class Part2(inputFilePath: String) {
         var password = 0
         for (line in lines){
             val rotation = Rotation.fromText(line)
+            val initialSequence = sequence
             sequence += rotation
             if(sequence > 99){
                 val (reduced, timesFullRotation) = reducePositive(sequence)
                 sequence = reduced
                 password += timesFullRotation
+                if(sequence == 0){
+                    password -= 1
+                }
             }
-            var reducedFromNegative = false
             if(sequence < 0){
                 val (reduced, timesFullRotation) = reduceNegative(sequence)
                 sequence = reduced
                 password += timesFullRotation
-                reducedFromNegative = true
+                if(initialSequence == 0){
+                    password -= 1
+                }
             }
-            println(sequence)
-            if(!reducedFromNegative && sequence == 0){
-                continue
-            }
+            println("Sequence: $sequence")
 
             if(sequence == 0){
                 password += 1
             }
-
+            println("Password: $password")
         }
 
         return password
     }
 
     fun reducePositive(number: Int): Pair<Int, Int> {
-        var reducedNumber = number
-        var timesReduced = 0
-        while(reducedNumber > 99){
-            reducedNumber -= 100
-            timesReduced += 1
-        }
-
+        val timesReduced = number / 100
+        val reducedNumber = number % 100
         return Pair(reducedNumber, timesReduced)
     }
 
     fun reduceNegative(number: Int): Pair<Int, Int> {
-        var reducedNumber = number
-        var timesReduced = 0
-        while(reducedNumber < 0){
-            reducedNumber += 100
-            timesReduced += 1
-        }
-
+        val timesReduced = (-number - 1) / 100 + 1
+        val reducedNumber = ((number % 100) + 100) % 100
         return Pair(reducedNumber, timesReduced)
     }
 
